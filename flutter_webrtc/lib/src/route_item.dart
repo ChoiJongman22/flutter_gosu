793db04ce819847f300ad23f3807ff2928419754
choi jong hun <jongman.urssu@gmail.com>
2021-04-01 00:11:43 +0900

    D_27

---
+++ flutter_webrtc/lib/src/route_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+typedef void RouteCallback(BuildContext context);
+
+class RouteItem {
+  RouteItem({
+    @required this.title,
+    @required this.subtitle,
+    @required this.push,
+  });
+
+  final String title;
+  final String subtitle;
+  final RouteCallback push;
+}
